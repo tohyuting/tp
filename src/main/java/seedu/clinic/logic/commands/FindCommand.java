bd9c0cea2da71dea7b167eff99fd0f31c6a9ba18
Zheng Wei <limzhengwei_99@hotmail.com>
2020-10-10 16:53:10 +0800

    Add functionality to find warehouses by products stored

--- src/main/java/seedu/clinic/logic/commands/FindCommand.java
+++ src/main/java/seedu/clinic/logic/commands/FindCommand.java
@@ -1,42 +1,65 @@
 package seedu.clinic.logic.commands;
 
+import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 
 import seedu.clinic.commons.core.Messages;
 import seedu.clinic.model.Model;
 import seedu.clinic.model.supplier.SupplierProductsContainKeywordsPredicate;
+import seedu.clinic.model.warehouse.WarehouseProductsContainKeywordsPredicate;
 
 /**
- * Finds and lists all suppliers in the CLI-nic app that sell products matching any of the argument keywords.
+ * Finds and lists all suppliers/warehouses in the CLI-nic app that sell/hold products matching any of the argument
+ * keywords.
  * Keyword matching is case insensitive.
+ * Keyword only matches whole word e.g. band will match "band aid" but not "bandaid".
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all suppliers that sell products matching "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all suppliers/warehouses that sell/hold products"
+            + "matching the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: TYPE KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " supplier panadol";
+            + "Example: " + COMMAND_WORD + " supplier panadol"
+            + "Example: " + COMMAND_WORD + " warehouse face mask";
 
-    private final SupplierProductsContainKeywordsPredicate predicate;
+    private final SupplierProductsContainKeywordsPredicate supplierPredicate;
+    private final WarehouseProductsContainKeywordsPredicate warehousePredicate;
 
-    public FindCommand(SupplierProductsContainKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    /**
+     * Constructs a new FindCommand object.
+     *
+     * @param supplierPredicate takes in the predicate used to filter a supplier's products.
+     * @param warehousePredicate takes in the predicate used to filter a warehouse's products.
+     */
+    public FindCommand(SupplierProductsContainKeywordsPredicate supplierPredicate,
+                       WarehouseProductsContainKeywordsPredicate warehousePredicate) {
+        this.supplierPredicate = supplierPredicate;
+        this.warehousePredicate = warehousePredicate;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredSupplierList(predicate);
-        return new CommandResult(
-                String.format(Messages.MESSAGE_SUPPLIERS_LISTED_OVERVIEW, model.getFilteredSupplierList().size()));
+
+        if (nonNull(supplierPredicate)) {
+            model.updateFilteredSupplierList(supplierPredicate);
+            return new CommandResult(
+                    String.format(Messages.MESSAGE_SUPPLIERS_LISTED_OVERVIEW, model.getFilteredSupplierList().size()));
+        } else {
+            model.updateFilteredWarehouseList(warehousePredicate);
+            return new CommandResult(
+                    String.format(Messages.MESSAGE_WAREHOUSE_LISTED_OVERVIEW, model.getFilteredWarehouseList().size()));
+        }
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof FindCommand // instanceof handles nulls
-                && predicate.equals(((FindCommand) other).predicate)); // state check
+                && (nonNull(supplierPredicate)
+                        ? supplierPredicate.equals(((FindCommand) other).supplierPredicate)
+                        : warehousePredicate.equals(((FindCommand) other).warehousePredicate)));
     }
 }
--- src/main/java/seedu/clinic/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/clinic/logic/parser/FindCommandParser.java
@@ -3,10 +3,12 @@ package seedu.clinic.logic.parser;
 import static seedu.clinic.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.List;
 
 import seedu.clinic.logic.commands.FindCommand;
 import seedu.clinic.logic.parser.exceptions.ParseException;
 import seedu.clinic.model.supplier.SupplierProductsContainKeywordsPredicate;
+import seedu.clinic.model.warehouse.WarehouseProductsContainKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -26,14 +28,20 @@ public class FindCommandParser implements Parser<FindCommand> {
         }
 
         String[] productNameKeywords = trimmedArgs.split("\\s+");
+        String type = productNameKeywords[0].toLowerCase();
 
         // Ensures that the user enters the type and at least 1 keyword
-        if (!productNameKeywords[0].toLowerCase().equals("supplier") || productNameKeywords.length < 2) {
+        if (!(type.equals("supplier") || type.equals("warehouse")) || productNameKeywords.length < 2) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        return new FindCommand(new SupplierProductsContainKeywordsPredicate(Arrays.asList(productNameKeywords)));
+        List<String> keywords = Arrays.asList(productNameKeywords);
+        if (type.equals("supplier")) {
+            return new FindCommand(new SupplierProductsContainKeywordsPredicate(keywords), null);
+        } else {
+            return new FindCommand(null, new WarehouseProductsContainKeywordsPredicate(keywords));
+        }
     }
 
 }
