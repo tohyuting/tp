73d595682e264e69bd2a58697a8d33a60a41757a
jeffreytjs <jeffreytan_1996@hotmail.com>
2020-10-11 14:03:58 +0800

    Include and modify test cases for AddCommand

--- src/test/java/seedu/clinic/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/clinic/logic/commands/AddCommandTest.java
@@ -1,9 +1,8 @@
 package seedu.clinic.logic.commands;
-/*
+
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.clinic.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
@@ -21,11 +20,10 @@ import seedu.clinic.model.Model;
 import seedu.clinic.model.ReadOnlyClinic;
 import seedu.clinic.model.ReadOnlyUserPrefs;
 import seedu.clinic.model.supplier.Supplier;
+import seedu.clinic.model.warehouse.Warehouse;
 import seedu.clinic.testutil.SupplierBuilder;
- */
 
 public class AddCommandTest {
-    /*
     @Test
     public void constructor_nullSupplier_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new AddCommand(null));
@@ -60,28 +58,26 @@ public class AddCommandTest {
         AddCommand addBobCommand = new AddCommand(bob);
 
         // same object -> returns true
-        assertTrue(addAliceCommand.equals(addAliceCommand));
+        assertEquals(addAliceCommand, addAliceCommand);
 
         // same values -> returns true
         AddCommand addAliceCommandCopy = new AddCommand(alice);
-        assertTrue(addAliceCommand.equals(addAliceCommandCopy));
+        assertEquals(addAliceCommandCopy, addAliceCommand);
 
         // different types -> returns false
-        assertFalse(addAliceCommand.equals(1));
+        assertNotEquals(addAliceCommand, 1);
 
         // null -> returns false
-        assertFalse(addAliceCommand.equals(null));
+        assertNotEquals(addAliceCommand, null);
 
         // different supplier -> returns false
-        assertFalse(addAliceCommand.equals(addBobCommand));
+        assertNotEquals(addBobCommand, addAliceCommand);
     }
-     */
 
     /*
      * A default model stub that have all of the methods failing.
      */
-    /*
-    private class ModelStub implements Model {
+    private static class ModelStub implements Model {
         @Override
         public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
             throw new AssertionError("This method should not be called.");
@@ -151,14 +147,42 @@ public class AddCommandTest {
         public void updateFilteredSupplierList(Predicate<Supplier> predicate) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public boolean hasWarehouse(Warehouse warehouse) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteWarehouse(Warehouse target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addWarehouse(Warehouse warehouse) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setWarehouse(Warehouse target, Warehouse editedWarehouse) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Warehouse> getFilteredWarehouseList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredWarehouseList(Predicate<Warehouse> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
-     */
 
     /*
      * A Model stub that contains a single supplier.
      */
-    /*
-    private class ModelStubWithSupplier extends ModelStub {
+    private static class ModelStubWithSupplier extends ModelStub {
         private final Supplier supplier;
 
         ModelStubWithSupplier(Supplier supplier) {
@@ -172,13 +196,11 @@ public class AddCommandTest {
             return this.supplier.isSameSupplier(supplier);
         }
     }
-     */
 
     /*
      * A Model stub that always accept the supplier being added.
      */
-    /*
-    private class ModelStubAcceptingSupplierAdded extends ModelStub {
+    private static class ModelStubAcceptingSupplierAdded extends ModelStub {
         final ArrayList<Supplier> suppliersAdded = new ArrayList<>();
 
         @Override
@@ -198,6 +220,5 @@ public class AddCommandTest {
             return new Clinic();
         }
     }
-     */
 
 }
