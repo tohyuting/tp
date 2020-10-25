fa81aef8dec37fab550d5e55a8b01ba9f848f806
tohyuting <tohyuting@gmail.com>
2020-10-25 22:06:20 +0800

    Edited Supplier class

--- src/main/java/seedu/clinic/logic/commands/EditCommand.java
+++ src/main/java/seedu/clinic/logic/commands/EditCommand.java
@@ -132,7 +132,7 @@ public class EditCommand extends Command {
                 throw new CommandException(MESSAGE_SUPPLIER_UNCHANGED);
             }
 
-            if (!supplierToEdit.isSameSupplierByName(editedSupplier) && model.hasSupplierByName(editedSupplier)) {
+            if (!supplierToEdit.isSameSupplier(editedSupplier) && model.hasSupplier(editedSupplier)) {
                 throw new CommandException(MESSAGE_DUPLICATE_SUPPLIER);
             }
 
