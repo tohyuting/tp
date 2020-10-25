7600025a06150dec6bb97c3a21a3ed41ff0ab775
qlchan24 <qlchan24@gmail.com>
2020-10-25 17:03:23 +0800

    Modify UpdateCommand, UpdateCommandParser
    - Add support for Supplier and all fields of product
    
    - Change implementation to check for optional product fields
    
    - Add new Descriptor and Descriptor Builder classes

--- src/main/java/seedu/clinic/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/clinic/logic/commands/UpdateCommand.java
@@ -1,74 +1,121 @@
 package seedu.clinic.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.clinic.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.clinic.logic.parser.CliSyntax.PREFIX_PRODUCT_NAME;
 import static seedu.clinic.logic.parser.CliSyntax.PREFIX_PRODUCT_QUANTITY;
-import static seedu.clinic.logic.parser.CliSyntax.PREFIX_WAREHOUSE_NAME;
+import static seedu.clinic.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.clinic.logic.parser.CliSyntax.PREFIX_TYPE;
+import static seedu.clinic.model.Model.PREDICATE_SHOW_ALL_SUPPLIERS;
 import static seedu.clinic.model.Model.PREDICATE_SHOW_ALL_WAREHOUSES;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
+import seedu.clinic.commons.util.CollectionUtil;
 import seedu.clinic.logic.commands.exceptions.CommandException;
+import seedu.clinic.logic.parser.Type;
 import seedu.clinic.model.Model;
 import seedu.clinic.model.attribute.Name;
+import seedu.clinic.model.attribute.Tag;
 import seedu.clinic.model.product.Product;
+import seedu.clinic.model.supplier.Supplier;
 import seedu.clinic.model.warehouse.Warehouse;
 
 /**
- * Update the stock for a product in a specific warehouse.
+ * Update the quantity/tags for a product in a specific warehouse/supplier, or adds the product if it does not
+ * already exist for that warehouse/supplier.
  */
 public class UpdateCommand extends Command {
 
     public static final String COMMAND_WORD = "update";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates a stock of a product in a specific warehouse. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates the quantity/tags of a product in a "
+            + "specific warehouse/supplier, or adds the product if it does not already exist for that "
+            + "warehouse/supplier."
             + "Parameters: "
-            + PREFIX_WAREHOUSE_NAME + "WAREHOUSE_NAME "
+            + PREFIX_TYPE + "TYPE "
+            + PREFIX_NAME + "WAREHOUSE/SUPPLIER NAME "
             + PREFIX_PRODUCT_NAME + "PRODUCT_NAME "
             + PREFIX_PRODUCT_QUANTITY + "PRODUCT_QUANTITY (Must be a non-negative integer)\n"
+            + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_WAREHOUSE_NAME + "Jurong Warehouse "
+            + PREFIX_TYPE + "w "
+            + PREFIX_NAME + "Jurong Warehouse "
             + PREFIX_PRODUCT_NAME + "Panadol "
-            + PREFIX_PRODUCT_QUANTITY + "350";
+            + PREFIX_PRODUCT_QUANTITY + "350 "
+            + PREFIX_TAG + "primary largest \n";
 
-    public static final String MESSAGE_SUCCESS = "Product stock updated: %1$sfor %2$s";
-    private static final String MESSAGE_NO_SUCH_WAREHOUSE = "The specified warehouse cannot be found";
+    public static final String MESSAGE_SUCCESS = "Product stock updated: %1$s for %2$s";
+    private static final String MESSAGE_NO_SUCH_ENTITY = "The specified warehouse/supplier cannot be found.";
+    private static final String MESSAGE_INVALID_TYPE = "Invalid Type.";
+    private static final String MESSAGE_EMPTY_DESCRIPTOR = "Either the quantity or tags (or both) has to be "
+            + "supplied to update an existing product";
 
-    private final Name warehouseName;
-    private final Product updatedProduct;
+    private final Type entityType;
+    private final Name entityName;
+    private final Name productName;
+    private final UpdateProductDescriptor updateProductDescriptor;
 
     /**
-     * Creates an UpdateCommand to update the specified {@code Product} for the warehouse with
-     * the specified {@code Name}
+     * Creates an UpdateCommand to update the product with the specified {@code productName} for the
+     * supplier/warehouse with the specified {@code entityName}
      */
-    public UpdateCommand(Name warehouseName, Product product) {
-        requireNonNull(warehouseName);
-        requireNonNull(product);
-        this.warehouseName = warehouseName;
-        updatedProduct = product;
+    public UpdateCommand(Type entityType, Name entityName, Name productName,
+            UpdateProductDescriptor updateProductDescriptor) {
+        requireNonNull(entityType);
+        requireNonNull(entityName);
+        requireNonNull(productName);
+        requireNonNull(updateProductDescriptor);
+
+        this.entityName = entityName;
+        this.productName = productName;
+        this.entityType = entityType;
+        this.updateProductDescriptor = updateProductDescriptor;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        switch (entityType) {
+        case SUPPLIER:
+            return updateProductForSupplier(model);
+        case WAREHOUSE:
+            return updateProductForWarehouse(model);
+        default:
+            throw new CommandException(MESSAGE_INVALID_TYPE);
+        }
+    }
+
+    private CommandResult updateProductForWarehouse(Model model) throws CommandException {
         Warehouse warehouseToUpdate;
 
         try {
-            warehouseToUpdate = getWarehouseByName(warehouseName, model);
+            warehouseToUpdate = getWarehouseByName(entityName, model);
         } catch (NoSuchElementException e) {
-            throw new CommandException(MESSAGE_NO_SUCH_WAREHOUSE);
+            throw new CommandException(MESSAGE_NO_SUCH_ENTITY);
         }
 
         Set<Product> updatedProductSet = new HashSet<>(warehouseToUpdate.getProducts());
+        Product productToUpdate;
 
-        if (updatedProductSet.contains(updatedProduct)) {
-            updatedProductSet.remove(updatedProduct); // removes the existing entry for the product
+        if (warehouseToUpdate.hasProductWithName(productName)) {
+            if (!updateProductDescriptor.isAnyFieldEdited()) {
+                throw new CommandException(MESSAGE_EMPTY_DESCRIPTOR);
+            }
+
+            productToUpdate = warehouseToUpdate.getProductByName(productName);
+            updatedProductSet.remove(productToUpdate); // removes the existing entry for the product
+        } else {
+            productToUpdate = new Product(productName, 0, new HashSet<>());
         }
 
-        updatedProductSet.add(updatedProduct); // adds the updated product
+        Product updatedProduct = createUpdatedProduct(productToUpdate, updateProductDescriptor);
+        updatedProductSet.add(updatedProduct);
         Warehouse updatedWarehouse = new Warehouse(warehouseToUpdate.getName(), warehouseToUpdate.getPhone(),
                 warehouseToUpdate.getAddress(), warehouseToUpdate.getRemark(), updatedProductSet);
         model.setWarehouse(warehouseToUpdate, updatedWarehouse);
@@ -77,21 +124,147 @@ public class UpdateCommand extends Command {
                 updatedWarehouse));
     }
 
+    private CommandResult updateProductForSupplier(Model model) throws CommandException {
+        Supplier supplierToUpdate;
+
+        try {
+            supplierToUpdate = getSupplierByName(entityName, model);
+        } catch (NoSuchElementException e) {
+            throw new CommandException(MESSAGE_NO_SUCH_ENTITY);
+        }
+
+        Set<Product> updatedProductSet = new HashSet<>(supplierToUpdate.getProducts());
+        Product productToUpdate;
+
+        if (supplierToUpdate.hasProductWithName(productName)) {
+            if (!updateProductDescriptor.isAnyFieldEdited()) {
+                throw new CommandException(MESSAGE_EMPTY_DESCRIPTOR);
+            }
+
+            productToUpdate = supplierToUpdate.getProductByName(productName);
+            updatedProductSet.remove(productToUpdate); // removes the existing entry for the product
+        } else {
+            productToUpdate = new Product(productName, 0, new HashSet<>());
+        }
+
+        Product updatedProduct = createUpdatedProduct(productToUpdate, updateProductDescriptor);
+        updatedProductSet.add(updatedProduct);
+        Supplier updatedSupplier = new Supplier(supplierToUpdate.getName(), supplierToUpdate.getPhone(),
+                supplierToUpdate.getEmail(), supplierToUpdate.getRemark(), updatedProductSet);
+        model.setSupplier(supplierToUpdate, updatedSupplier);
+        model.updateFilteredSupplierList(PREDICATE_SHOW_ALL_SUPPLIERS);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, updatedProduct.toStringForSupplier(),
+                updatedSupplier));
+    }
+
+    /**
+     * Creates and returns a {@code Product} with the details of {@code productToUpdate}
+     * updated with {@code updateProductDescriptor}.
+     */
+    private static Product createUpdatedProduct(Product productToUpdate,
+            UpdateProductDescriptor updateProductDescriptor) {
+        assert productToUpdate != null;
+
+        int updatedQuantity = updateProductDescriptor.getQuantity().orElse(productToUpdate.getProductQuantity());
+        Set<Tag> updatedTags = updateProductDescriptor.getTags().orElse(productToUpdate.getProductTags());
+
+        return new Product(productToUpdate.getProductName(), updatedQuantity, updatedTags);
+    }
+
     public static Warehouse getWarehouseByName(Name warehouseName, Model model) throws NoSuchElementException {
         return model.getClinic().getWarehouseList().stream()
                 .filter(warehouse -> warehouse.getName().equals(warehouseName)).findFirst().orElseThrow();
+    }
+
+    public static Supplier getSupplierByName(Name supplierName, Model model) throws NoSuchElementException {
+        return model.getClinic().getSupplierList().stream()
+                .filter(supplier -> supplier.getName().equals(supplierName)).findFirst().orElseThrow();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof UpdateCommand // instanceof handles nulls
-                && warehouseName.equals(((UpdateCommand) other).warehouseName)
-                && updatedProduct.equals(((UpdateCommand) other).updatedProduct));
+                && entityType.equals(((UpdateCommand) other).entityType)
+                && entityName.equals(((UpdateCommand) other).entityName)
+                && productName.equals(((UpdateCommand) other).productName)
+                && updateProductDescriptor.equals(((UpdateCommand) other).updateProductDescriptor));
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(warehouseName, updatedProduct);
+        return Objects.hash(entityType, entityName, productName, updateProductDescriptor);
     }
+
+    /**
+     * Stores the details to edit the product with. Each non-empty field value will replace the
+     * corresponding field value of the product.
+     */
+    public static class UpdateProductDescriptor {
+        private Integer quantity;
+        private Set<Tag> tags;
+
+        public UpdateProductDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public UpdateProductDescriptor(UpdateProductDescriptor toCopy) {
+            setQuantity(toCopy.quantity);
+            setTags(toCopy.tags);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(quantity, tags);
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+        public Optional<Integer> getQuantity() {
+            return Optional.ofNullable(quantity);
+        }
+
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public void setTags(Set<Tag> tags) {
+            this.tags = (tags != null) ? new HashSet<>(tags) : null;
+        }
+
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
+        public Optional<Set<Tag>> getTags() {
+            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof UpdateProductDescriptor)) {
+                return false;
+            }
+
+            // state check
+            UpdateProductDescriptor e = (UpdateProductDescriptor) other;
+
+            return getQuantity().equals(e.getQuantity())
+                    && getTags().equals(e.getTags());
+        }
+    }
+
 }
