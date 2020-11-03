6c1044b74e34fa03fbc60c322c424a12df0438bc
Zheng Wei <limzhengwei_99@hotmail.com>
2020-11-04 01:40:59 +0800

    Add Javadoc for CommandHistory

--- src/main/java/seedu/clinic/model/CommandHistory.java
+++ src/main/java/seedu/clinic/model/CommandHistory.java
@@ -12,11 +12,18 @@ public class CommandHistory {
 
     public CommandHistory() {};
 
+    /**
+     * Constructs a new CommandHistory wrapper that wraps data read from commandHistory.txt.
+     * @param commandHistory takes in a list of command history strings.
+     */
     public CommandHistory(List<String> commandHistory) {
         this.commandHistory = commandHistory;
         commandHistoryIndex = commandHistory.size();
     }
 
+    /**
+     * Returns the command previously entered by the user.
+     */
     public String readPreviousHistory() {
         String prevHistory = "";
         if (commandHistoryIndex > 0) {
@@ -31,6 +38,9 @@ public class CommandHistory {
         return prevHistory;
     }
 
+    /**
+     * Returns an earlier command that was previously entered by the user.
+     */
     public String readNextHistory() {
         String nextHistory = "";
 
@@ -46,6 +56,15 @@ public class CommandHistory {
         return nextHistory;
     }
 
+    /**
+     * Updates the list of history commands with a new command just entered by the user.
+     * Note that this function does not save the command directly to commandHistory.txt file.
+     * This function merely updates the CommandHistory model with the latest command.
+     * The command just entered by the user will still be saved to commandHistory.txt but it will be handled by a
+     * different function and class.
+     *
+     * @param newCommandHistory takes in the command just entered by the user.
+     */
     public void updateHistory(String newCommandHistory) {
         commandHistory.add(newCommandHistory);
         commandHistoryIndex = commandHistory.size();
