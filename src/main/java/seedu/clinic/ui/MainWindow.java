e6262e0b39ea583f70cd268672609a59f09e3f22
jeffreytjs <jeffreytjs1996@gmail.com>
2020-11-04 15:18:09 +0800

    Remove unused code

--- src/main/java/seedu/clinic/ui/MainWindow.java
+++ src/main/java/seedu/clinic/ui/MainWindow.java
@@ -33,7 +33,6 @@ public class MainWindow extends UiPart<Stage> {
     // Independent Ui parts residing in this Ui container
     private SupplierListPanel supplierListPanel;
     private WarehouseListPanel warehouseListPanel;
-    // private SupplierListPanel supplierListPanel2;
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
 
@@ -48,7 +47,6 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane warehouseListPanelPlaceholder;
-    //private StackPane supplierListPanelPlaceholder2;
 
     @FXML
     private StackPane resultDisplayPlaceholder;
@@ -118,8 +116,6 @@ public class MainWindow extends UiPart<Stage> {
 
         warehouseListPanel = new WarehouseListPanel(logic.getFilteredWarehouseList());
         warehouseListPanelPlaceholder.getChildren().add(warehouseListPanel.getRoot());
-        // supplierListPanel2 = new SupplierListPanel(logic.getFilteredSupplierList());
-        // supplierListPanelPlaceholder2.getChildren().add(supplierListPanel2.getRoot());
 
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
