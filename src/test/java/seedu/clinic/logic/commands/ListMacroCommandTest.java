ac97103ad1ed5fb579b2510e0a9c4ffe8b68b1e8
qlchan24 <qlchan24@gmail.com>
2020-11-05 14:25:03 +0800

    Fix bugs in merge

--- src/test/java/seedu/clinic/logic/commands/ListMacroCommandTest.java
+++ src/test/java/seedu/clinic/logic/commands/ListMacroCommandTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.clinic.logic.commands.exceptions.CommandException;
 import seedu.clinic.model.Clinic;
+import seedu.clinic.model.CommandHistory;
 import seedu.clinic.model.Model;
 import seedu.clinic.model.ModelManager;
 import seedu.clinic.model.UserMacros;
@@ -17,14 +18,15 @@ public class ListMacroCommandTest {
 
     @Test
     public void execute_listMacroWithEmptyList_throwCommandException() {
-        Model emptyModel = new ModelManager(new Clinic(), new UserPrefs(), new UserMacros());
+        Model emptyModel = new ModelManager(new Clinic(), new UserPrefs(), new UserMacros(), new CommandHistory());
         assertThrows(CommandException.class, () -> new ListMacroCommand().execute(emptyModel));
     }
 
     @Test
     public void execute_listMacro_success() {
-        Model model = new ModelManager(new Clinic(), new UserPrefs(), getTypicalUserMacros());
-        Model expectedModel = new ModelManager(new Clinic(), new UserPrefs(), model.getUserMacros());
+        Model model = new ModelManager(new Clinic(), new UserPrefs(), getTypicalUserMacros(), new CommandHistory());
+        Model expectedModel = new ModelManager(new Clinic(), new UserPrefs(), model.getUserMacros(),
+                new CommandHistory());
         String expectedListString = "1. as\nSaved Command String: add ct/s\n\n"
                 + "2. aw\nSaved Command String: add ct/w\n\n"
                 + "3. us\nSaved Command String: update ct/s\n\n"
